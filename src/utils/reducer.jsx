d0d214876711a1b5c3a9305c594aa1a9df0333ea
faustabmnyh <faustaabimanyu@gmail.com>
2021-05-26 22:55:57 +0700

    little clean up the responsive design

--- src/utils/reducer.jsx
+++ src/utils/reducer.jsx
@@ -12,7 +12,7 @@ const reducer = (state, action) => {
     case "ADD_TO_BASKET":
       return {
         ...state, 
-        basket: [...state.basket, action.item],
+        basket: [action.item, ...state.basket],
       };
 
     case "REMOVE_FROM_BASKET":
